b9b3475a91dfb3b5809dcec40efb9ab878a99cc6
qhj <me@qhj.moe>
2023-12-24 00:25:19 +0800

    chore: nix develop (nix flakes)

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Nix-flake-based Go development environment";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self , nixpkgs ,... }:
+  let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}" = {
+      default =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          fish
+          go
+        ];
+
+        shellHook = with pkgs; ''
+          exec fish
+        '';
+      };
+    };
+  };
+}
